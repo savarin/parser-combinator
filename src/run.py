5c53d0eb66f91ce5e7083ea173de797e1b622636
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-03-08 16:58:41 -0800

    include nothing

--- src/run.py
+++ src/run.py
@@ -5,11 +5,17 @@ def shift(source: str) -> Union[Tuple[str, str], bool]:
     return bool(source) and (source[0], source[1:])
 
 
+def nothing(source: str) -> Tuple[None, str]:
+    return (None, "bar")
+
+
 def test_run() -> None:
     assert shift("bar") == ("b", "ar")
     assert shift("ar") == ("a", "r")
     assert shift("r") == ("r", "")
     assert shift("") is False
+
+    assert nothing("bar") == (None, "bar")
 
 
 if __name__ == "__main__":
