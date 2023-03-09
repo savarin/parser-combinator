fef451eb76b74e5d633bbc334020f540d9fa7e35
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-03-08 17:48:59 -0800

    include sieve

--- src/run.py
+++ src/run.py
@@ -1,12 +1,29 @@
-from typing import Tuple, Union
+from typing import Callable, Literal, Tuple, Union
 
 
-def shift(source: str) -> Union[Tuple[str, str], bool]:
+Pair = Union[Tuple[str, str], Literal[False]]
+PairCallable = Callable[[str], Pair]
+
+
+def shift(source: str) -> Pair:
     return bool(source) and (source[0], source[1:])
 
 
 def nothing(source: str) -> Tuple[None, str]:
     return (None, "bar")
+
+
+def sieve(predicate: Callable[[str], bool]) -> Callable[[PairCallable], PairCallable]:
+    def f(parser: PairCallable) -> PairCallable:
+        def g(source: str) -> Pair:
+            def h(result: Pair) -> Pair:
+                return result and predicate(result[0]) and result
+
+            return h(parser(source))
+
+        return g
+
+    return f
 
 
 def test_run() -> None:
@@ -16,6 +33,9 @@ def test_run() -> None:
     assert shift("") is False
 
     assert nothing("bar") == (None, "bar")
+
+    assert sieve(str.isdigit)(shift)("456") == ("4", "56")
+    assert sieve(str.isalpha)(shift)("456") is False
 
 
 if __name__ == "__main__":
